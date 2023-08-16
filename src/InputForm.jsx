6611618f3f04eb60024e61b3c4edb825a30018c5
Veis1337 <117036853+Veis1337@users.noreply.github.com>
2023-08-16 13:44:12 -0400

    added mean options

--- src/InputForm.jsx
+++ src/InputForm.jsx
@@ -31,7 +31,7 @@ Each response should follow this structure:
 2. Introduce any characters besides the User present with detailed descriptions.
 3. For character dialogue, use quotation marks and provide meaningful responses.
 4. Italicize character actions to make them stand out.
-5. Provide five (not six!) different options for the user to choose from, numbered 1 to 5. Make the 5th selection a bizarre scenario that could shake up the story.
+5. Provide five (not six!) different options for the user to choose from, numbered 1 to 5. Make the 4th selection a particularly mean option. Make the 5th selection a bizarre scenario that could change the story entirely.
 6. Make the options diverse, including actions, dialogue, or unexpected events.
 7. The user will reply with a number to select their chosen option to drive the adventure forward.
 8. ONLY reference the option that the user selected.  All other options should be discarded from the history of the roleplay and never referenced. 
